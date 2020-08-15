f22c5dd19e69f7f13bf14ec88434d2dc5247bd3f
subodh3kumar <subodh3kumar@gmail.com>
2020-08-16 01:47:39 +0530

    JavaCodingProblems

---
+++ JavaCodingProblems/src/main/java/chapter01/problem01/DuplicateCount.java
@@ -0,0 +1,13 @@
+package chapter01.problem01;
+
+import java.util.Map;
+
+public class DuplicateCount {
+
+    public Map<Character, Integer> countDuplicateChars(String string) {
+        if (string == null || string.trim().equals("")) {
+            throw new NullPointerException("string is null");
+        }
+        return null;
+    }
+}
